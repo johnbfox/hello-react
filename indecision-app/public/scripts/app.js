32529d0d6513d3b6f8d929a0dec2def0a1c2fce2
John Fox <johnfox@Johns-MacBook-Pro.local>
2019-12-12 21:17:11 -0500

    Conditional rendering challenge

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -3,12 +3,62 @@
 console.log('App.js is running');
 
 var heya = 'wassup';
-// JSX - JavaScript XML
-var template = React.createElement(
-  'h1',
-  null,
-  'heya'
+
+var app = {
+    title: 'Star Wars',
+    options: []
+
+    // JSX - JavaScript XML
+};var template = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        app.title
+    ),
+    app.subtitle && React.createElement(
+        'p',
+        null,
+        app.subtitle
+    ),
+    React.createElement(
+        'p',
+        null,
+        app.options.length > 0 ? 'Here are your options' : 'No options'
+    )
 );
+
+var user = {
+    name: 'John',
+    age: 27,
+    location: 'Boston'
+};
+
+function getLocation(location) {
+    return location ? React.createElement(
+        'p',
+        null,
+        location
+    ) : undefined;
+}
+var templateTwo = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        user.name || 'Anonymous'
+    ),
+    user.age && user.age >= 18 && React.createElement(
+        'p',
+        null,
+        'Age: ',
+        user.age
+    ),
+    getLocation(user.location)
+);
+
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,8 +1,39 @@
 console.log('App.js is running');
 
 const heya = 'wassup';
+
+const app = {
+    title: 'Star Wars',
+    options: []
+}
+
 // JSX - JavaScript XML
-var template = <h1>heya</h1>;
+var template = (
+    <div>
+        <h1>{app.title}</h1>
+        {app.subtitle && <p>{app.subtitle}</p>}
+        <p>{(app.options.length > 0) ? 'Here are your options' : 'No options'}</p>
+    </div>
+);
+
+
+const user = {
+    name: 'John',
+    age: 27,
+    location: 'Boston'
+}
+
+function getLocation(location) {
+    return (location) ? <p>{location}</p> : undefined;
+}
+var templateTwo = (
+    <div>
+        <h1>{user.name || 'Anonymous'}</h1>
+        {(user.age && user.age >= 18) && <p>Age: { user.age}</p>}
+        {getLocation(user.location)}
+    </div>
+)
+
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot)
+ReactDOM.render(template, appRoot);
